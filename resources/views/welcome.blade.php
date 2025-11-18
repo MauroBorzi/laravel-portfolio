4c3a6285fc9cee2ff39a583840e70ecdc1e26ef3
MauroBorzi <mauroborzi91@gmail.com>
2025-11-18 10:25:09 +0100

    Add create view for posts and update PostController to return the view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
     <div class="container">
         <div class="row">
             <div class="col-12 text-center mt-4">
-                <h1>IL MIO PORTFOLIO</h1>
-                <h4 class="text-secondary mb-4">Ciao! Io sono Mauro e questo è il mio portfolio! =D</h4>
+                <h1>LA MIA BACHECA</h1>
+                <h4 class="text-secondary mb-4">Ciao! Io sono Mauro e questa è la mia bacheca! =D</h4>
 
                 <!-- Immagine -->
                 <img src="{{ asset('images/portfolio-image.jpg') }}" alt="Portfolio Image" class="img-fluid rounded shadow-lg"
